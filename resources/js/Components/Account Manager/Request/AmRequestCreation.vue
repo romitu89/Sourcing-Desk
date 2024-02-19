375325dd104c3e185802cf1ffb283a1b10e56d8d
romit <romitu89@gmail.com>
2024-02-19 17:34:22 +0530

    SO-DE-068 : in progress

--- resources/js/Components/Account Manager/Request/AmRequestCreation.vue
+++ resources/js/Components/Account Manager/Request/AmRequestCreation.vue
@@ -1,10 +1,32 @@
 <template>
-    <h2>Am Request Creation page</h2>
-</template>
+    <form @submit.prevent="submitForm">
+     <table class="input_form">
+
+
+     <tr>
+     <td ><label >Select Location</label></td>
+     <td ><select id="location"  name="location">
+     <option value="">Select Location</option>
+     <option v-for="item in userLocation" :key='item.location' :value="item.location">{{ item.location }}</option>
+
+     </select></td>
+     </tr>
+
+     <tr>
+         <td></td>
+        <td>
+         <button  class="submit_btn">Submit</button> </td>
+     </tr>
+
+     </table>
+
+    </form>
+
+ </template>
 
 <script>
     export default {
         name:'AmRequestCreation',
-        
+
     }
 </script>
