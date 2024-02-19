24338b52b0d331b1958f622badf00461aad1d1e1
romit <romitu89@gmail.com>
2024-02-19 18:15:35 +0530

    SO-DE-068: form complete

--- resources/js/Components/Account Manager/Submission/AmSubmissionRequest.vue
+++ resources/js/Components/Account Manager/Submission/AmSubmissionRequest.vue
@@ -1,12 +1,31 @@
 <template>
-    <h2>AmSubmissionRequest page</h2>
+    <form @submit.prevent="submitForm">
+     <table class="input_form">
+     <tr>
+     <td ><label >Select Location</label></td>
+     <td ><select id="location"  name="location">
+     <option value="">Select Location</option>
+     <option v-for="item in userLocation" :key='item.location' :value="item.location">{{ item.location }}</option>
 
-</template>
+     </select></td>
+     </tr>
+
+     <tr>
+         <td></td>
+        <td>
+         <button  class="submit_btn">Submit</button> </td>
+     </tr>
+
+     </table>
+
+    </form>
+
+ </template>
 
 <script>
     export default {
-        
+
         name:'AmSubmissionRequest',
-        
+
     }
 </script>
