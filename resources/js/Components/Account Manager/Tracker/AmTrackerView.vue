f934ce3cb34ff648b22238471b343b57b44efddd
romit <romitu89@gmail.com>
2024-05-23 09:03:21 +0530

    SO-DE-128: tracker duplicate done

--- resources/js/Components/Account Manager/Tracker/AmTrackerView.vue
+++ resources/js/Components/Account Manager/Tracker/AmTrackerView.vue
@@ -6,7 +6,7 @@
           <td>
             <select v-model="tracker.selectedLocation" @change="fetchClients" @blur="checkValidation('selectedLocation')" name="location">
               <option value="">Select Location</option>
-              <option v-for="item in userLocation" :key="item.location" :value="item.location">{{ item.location }}</option>
+              <option v-for="item in uniqueLocations" :key="item" :value="item">{{ item }}</option>
             </select><br>
             <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
           </td>
@@ -17,7 +17,7 @@
           <td>
             <select v-model="tracker.selectedClient" @change="fetchBusinessUnits" @blur="checkValidation('selectedClient')" name="client">
               <option value="">Select Client</option>
-              <option v-for="client in clients" :key="client.client_name" :value="client.client_name">{{ client.client_name }}</option>
+              <option v-for="client in uniqueClients" :key="client" :value="client">{{ client }}</option>
             </select><br>
             <span v-if="errors.selectedClient" class="error">{{ errors.selectedClient[0] }}</span>
           </td>
@@ -28,7 +28,7 @@
           <td>
             <select v-model="tracker.selectedBusiness" @change="fetchClientManagers" @blur="checkValidation('selectedBusiness')" name="business">
               <option value="">Select Unit</option>
-              <option v-for="client in clients" :key="client.business_unit" :value="client.business_unit">{{ client.business_unit }}</option>
+              <option v-for="unit in uniqueBusinessUnits" :key="unit" :value="unit">{{ unit }}</option>
             </select><br>
             <span v-if="errors.selectedBusiness" class="error">{{ errors.selectedBusiness[0] }}</span>
           </td>
@@ -39,7 +39,7 @@
           <td>
             <select v-model="tracker.selectedManager" @blur="checkValidation('selectedManager')" name="manager">
               <option value="">Select Manager</option>
-              <option v-for="client in clients" :key="client.client_manager_name" :value="client.client_manager_name">{{ client.client_manager_name }}</option>
+              <option v-for="manager in uniqueManagers" :key="manager" :value="manager">{{ manager }}</option>
             </select><br>
             <span v-if="errors.selectedManager" class="error">{{ errors.selectedManager[0] }}</span>
           </td>
@@ -57,167 +57,178 @@
 
     <div v-if="results">
       <responsive-table :results="results" :columns="columns" :buttonAction="buttonAction">
-      <template #edit="{ row }">
-        <div>
-          <button title="Edit" @click="editItem(row.id)">
-            <font-awesome-icon :icon="['fas', 'pen-to-square']" />
-          </button>
-          <button title="Delete" @click="deleteItem(row.id)">
-            <font-awesome-icon :icon="['fas', 'trash']" />
-          </button>
-        </div>
-      </template>
+        <template #edit="{ row }">
+          <div>
+            <button title="Edit" @click="editItem(row.id)">
+              <font-awesome-icon :icon="['fas', 'pen-to-square']" />
+            </button>
+            <button title="Delete" @click="deleteItem(row.id)">
+              <font-awesome-icon :icon="['fas', 'trash']" />
+            </button>
+          </div>
+        </template>
       </responsive-table>
     </div>
   </template>
 
   <script>
   import { commonFunctionsMixin } from '../../../function.js';
-import axios from 'axios';
-import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue';
+  import axios from 'axios';
+  import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue';
 
-export default {
-  name: 'AmTrackerView',
-  mixins:[commonFunctionsMixin],
-  components: {
-    ResponsiveTable,
-  },
-  data() {
-    return {
-      buttonAction: false,
-      tracker: {
-        selectedLocation: '',
-        selectedClient: '',
-        selectedBusiness: '',
-        selectedManager: '',
-      },
-      userLocation: [],
-      clients: [],
-      errors: {},
-      results: [],
-      columns: [
-        { label: 'Client Name', key: 'client_name' },
-        { label: 'Client Manager Name', key: 'client_manager_name' },
-        { label: 'Business Unit', key: 'business_unit' },
-        { label: 'Select Location', key: 'location' },
-        { label: 'Edit', key: 'edit' },
-      ],
-      empNameProp:this.empName
-    };
-  },
-  props:{
-          empName:{
-            type: String,
-            default: "",
-          },
+  export default {
+    name: 'AmTrackerView',
+    mixins: [commonFunctionsMixin],
+    components: {
+      ResponsiveTable,
+    },
+    data() {
+      return {
+        buttonAction: false,
+        tracker: {
+          selectedLocation: '',
+          selectedClient: '',
+          selectedBusiness: '',
+          selectedManager: '',
         },
-  methods: {
-    closePopup() {
-      this.$emit('closePopup');
+        userLocation: [],
+        clients: [],
+        errors: {},
+        results: [],
+        columns: [
+          { label: 'Client Name', key: 'client_name' },
+          { label: 'Client Manager Name', key: 'client_manager_name' },
+          { label: 'Business Unit', key: 'business_unit' },
+          { label: 'Select Location', key: 'location' },
+          { label: 'Edit', key: 'edit' },
+        ],
+        empNameProp: this.empName,
+      };
     },
-    editItem(id) {
-      this.$emit("updateForm", id);
-    },
-    checkValidation(fieldName) {
-      if (this.errors.hasOwnProperty(fieldName)) {
-        delete this.errors[fieldName];
-      }
-    },
-    fetchClients() {
-      const selectedLocation = this.tracker.selectedLocation;
-      if (selectedLocation) {
-        // Fetch clients based on selected location
-        this.clients = this.userLocation.filter(item => item.location === selectedLocation);
-      } else {
-        this.clients = [];
-      }
-      // Reset subsequent selections
-      this.tracker.selectedClient = '';
-      this.tracker.selectedBusiness = '';
-      this.tracker.selectedManager = '';
-    },
-    fetchBusinessUnits() {
-      const selectedClientName = this.tracker.selectedClient;
-      if (selectedClientName) {
-        // Filter clients to find the selected client
-        const selectedClient = this.clients.find(client => client.client_name === selectedClientName);
-        if (selectedClient) {
-          // Set the business units based on the selected client
-          this.clients = [selectedClient]; // Update clients array with selected client for consistency
-        } else {
-          this.clients = []; // No matching client found, clear the array
-        }
-      } else {
-        this.clients = []; // Reset clients if no client is selected
-      }
-    },
-    fetchClientManagers() {
-      // Logic to fetch client managers based on selected business unit
-    },
-    userLocationApi() {
-      axios.get('/api/amtracker-view')
-        .then(response => {
-          this.userLocation = response.data.client;
-          console.log(this.userLocation); // Check the data in console
-        })
-        .catch(error => {
-          console.error('Error fetching user location:', error);
-          this.errored = true;
-        });
-    },
-    resetForm() {
-      this.tracker.selectedLocation = '';
-      this.tracker.selectedClient = '';
-      this.tracker.selectedBusiness = '';
-      this.tracker.selectedManager = '';
-      this.errors = {};
-    },
-    submitForm() {
-      this.submitted = true;
-      axios.post('/api/amtracker-view', this.tracker)
-        .then(response => {
-          this.results = response.data.results;
-          if (Object.values(this.errors).length === 0) {
-            this.buttonAction = true;
-          }
-        })
-        .catch(error => {
-          console.error('Error submitting form:', error.response.data.errors);
-          this.errors = error.response.data.errors;
-        });
-      this.resetForm();
-    },
-    clearMessage(){
-      this.empNameProp = ""
-
-    },
-    showSucess(){
-      if(this.empNameProp!=""){
-      Swal.fire({
-              position: "top-center",
-              icon: "success",
-              title: "User "+this.empNameProp+" edited successfully",
-              showConfirmButton: false,
-              timer: 3000,
-            });
-
-    }
-    },
-},
-
-  mounted() {
-    this.userLocationApi();
-  },
-  watch: {
-    empName(newVal){
-            this.empNameProp = newVal
-          },
-    userLocation: {
-      handler() {
-        this.tracker.selectedLocation = ''; // Reset selected location
+    props: {
+      empName: {
+        type: String,
+        default: "",
       },
-      deep: true
     },
-  },
-};
-</script>
+    computed: {
+      uniqueLocations() {
+        return [...new Set(this.userLocation.map(item => item.location))];
+      },
+      uniqueClients() {
+        return [...new Set(this.clients.map(item => item.client_name))];
+      },
+      uniqueBusinessUnits() {
+        return [...new Set(this.clients.map(item => item.business_unit))];
+      },
+      uniqueManagers() {
+        return [...new Set(this.clients.map(item => item.client_manager_name))];
+      },
+    },
+    methods: {
+      closePopup() {
+        this.$emit('closePopup');
+      },
+      editItem(id) {
+        this.$emit("updateForm", id);
+      },
+      checkValidation(fieldName) {
+        if (this.errors.hasOwnProperty(fieldName)) {
+          delete this.errors[fieldName];
+        }
+      },
+      fetchClients() {
+        const selectedLocation = this.tracker.selectedLocation;
+        if (selectedLocation) {
+          // Fetch clients based on selected location
+          this.clients = this.userLocation.filter(item => item.location === selectedLocation);
+        } else {
+          this.clients = [];
+        }
+        // Reset subsequent selections
+        this.tracker.selectedClient = '';
+        this.tracker.selectedBusiness = '';
+        this.tracker.selectedManager = '';
+      },
+      fetchBusinessUnits() {
+        const selectedClientName = this.tracker.selectedClient;
+        if (selectedClientName) {
+          // Filter clients to find the selected client
+          const selectedClient = this.clients.find(client => client.client_name === selectedClientName);
+          if (selectedClient) {
+            // Set the business units based on the selected client
+            this.clients = [selectedClient]; // Update clients array with selected client for consistency
+          } else {
+            this.clients = []; // No matching client found, clear the array
+          }
+        } else {
+          this.clients = []; // Reset clients if no client is selected
+        }
+      },
+      fetchClientManagers() {
+        // Logic to fetch client managers based on selected business unit
+      },
+      userLocationApi() {
+        axios.get('/api/amtracker-view')
+          .then(response => {
+            this.userLocation = response.data.client;
+            console.log(this.userLocation); // Check the data in console
+          })
+          .catch(error => {
+            console.error('Error fetching user location:', error);
+            this.errored = true;
+          });
+      },
+      resetForm() {
+        this.tracker.selectedLocation = '';
+        this.tracker.selectedClient = '';
+        this.tracker.selectedBusiness = '';
+        this.tracker.selectedManager = '';
+        this.errors = {};
+      },
+      submitForm() {
+        this.submitted = true;
+        axios.post('/api/amtracker-view', this.tracker)
+          .then(response => {
+            this.results = response.data.results;
+            if (Object.values(this.errors).length === 0) {
+              this.buttonAction = true;
+            }
+          })
+          .catch(error => {
+            console.error('Error submitting form:', error.response.data.errors);
+            this.errors = error.response.data.errors;
+          });
+        this.resetForm();
+      },
+      clearMessage() {
+        this.empNameProp = ""
+      },
+      showSucess() {
+        if (this.empNameProp != "") {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "User " + this.empNameProp + " edited successfully",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        }
+      },
+    },
+    mounted() {
+      this.userLocationApi();
+    },
+    watch: {
+      empName(newVal) {
+        this.empNameProp = newVal
+      },
+      userLocation: {
+        handler() {
+          this.tracker.selectedLocation = ''; // Reset selected location
+        },
+        deep: true
+      },
+    },
+  };
+  </script>
