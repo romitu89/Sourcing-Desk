a1fb3cdb50bbc6c0399cecaa3a6ebfc1bf6a6c14
romit <romitu89@gmail.com>
2024-05-30 19:58:23 +0530

    tracker and team done

--- resources/js/Components/Account Manager/Team Management/AmTeamManagementCreate.vue
+++ resources/js/Components/Account Manager/Team Management/AmTeamManagementCreate.vue
@@ -12,57 +12,55 @@
                 :value="item.location"
               >
                 {{ item.location }}
-              </option></select
-            ><br /><span v-if="errors.selectedLocation" class="error">{{
-              errors.selectedLocation[0]
-            }}</span>
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
           </td>
         </tr>
 
-        <tr>
+        <tr v-if="teamManager.selectedLocation">
           <td><label>Client Name</label></td>
           <td>
-            <select id="role" v-model="teamManager.clientName" @blur="checkValidation('jobType')" name="role">
+            <select id="clientName" v-model="teamManager.clientName" @blur="checkValidation('clientName')" name="clientName">
               <option value="">Select Client</option>
-              <option
-                v-for="item in client"
-                :key="item.client_name"
-                :value="item.client_name"
-              >
+              <option v-for="item in client" :key="item.client_name" :value="item.client_name">
                 {{ item.client_name }}
-              </option></select
-            ><br /><span v-if="errors.clientName" class="error">{{
-              errors.clientName[0]
-            }}</span>
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.clientName" class="error">{{ errors.clientName[0] }}</span>
           </td>
         </tr>
 
-        <tr>
+        <tr v-if="teamManager.selectedLocation && teamManager.clientName">
           <td><label>Job Type</label></td>
           <td>
-            <select id="role" v-model="teamManager.jobType" @blur="checkValidation('jobType')" name="role">
+            <select id="jobType" v-model="teamManager.jobType" @blur="checkValidation('jobType')" name="jobType">
               <option value="">Select Job</option>
               <option value="permanent">Permanent</option>
               <option value="contract">Contract</option>
-              <option value="both">Both</option></select
-            ><br /><span v-if="errors.jobType" class="error">{{ errors.jobType[0] }}</span>
+              <option value="both">Both</option>
+            </select>
+            <br />
+            <span v-if="errors.jobType" class="error">{{ errors.jobType[0] }}</span>
           </td>
         </tr>
 
-        <tr>
+        <tr v-if="teamManager.selectedLocation && teamManager.clientName && teamManager.jobType">
           <td><label>Select Team</label></td>
           <td>
             <multi-select
               :selectedTeam="teamManager.selectedTeam"
               :options="teams"
-              @update:selected="updateSelectedOptions" @blur="checkValidation('selectedTeam')"
+              @update:selected="updateSelectedOptions"
+              @blur="checkValidation('selectedTeam')"
             >
             </multi-select>
-            <span v-if="errors.selectedTeam" class="error">{{
-              errors.selectedTeam[0]
-            }}</span>
+            <span v-if="errors.selectedTeam" class="error">{{ errors.selectedTeam[0] }}</span>
           </td>
         </tr>
+
         <tr>
           <td></td>
           <td>
@@ -77,7 +75,8 @@
   <script>
   import { commonFunctionsMixin } from '../../../function.js';
   import MultiSelect from "../../Shared Folder/MultiSelect.vue";
-  import Swal from 'sweetalert2'
+  import Swal from 'sweetalert2';
+  import axios from 'axios';
 
   export default {
     name: "AmTeamManagementCreate",
@@ -105,26 +104,24 @@
         this.$emit("closePopup");
       },
 
-
       userLocationApi() {
         axios
           .get("/api/amteam-create")
           .then((response) => {
-            console.log(response.data.location, "response.data.location");
             this.location = response.data.location;
             this.client = response.data.client
 
-            const teamEmail = response.data.teamEmail; // Corrected variable name to match your initial question
+            const teamEmail = response.data.teamEmail;
 
             teamEmail.forEach((tm) => {
               this.teams.push({
-                label: tm.email_id, // Display email as the label
-                value: tm.email_id, // Use team ID as the value
+                label: tm.email_id,
+                value: tm.email_id,
               });
             });
           })
           .catch((error) => {
-            console.log(error);
+            console.error(error);
             this.errored = true;
           });
       },
@@ -136,8 +133,7 @@
       },
 
       submitForm() {
-        this.submitted = true; // Set the submitted flag to true when attempting to submit the form
-        // if (this.isFormValid) {
+        this.submitted = true;
 
         axios
           .post("/api/amteam-create", this.teamManager)
@@ -155,14 +151,11 @@
             } else {
               Swal.fire("Form not Submitted");
             }
-
-            // Handle the response as needed
           })
           .catch((error) => {
             if (error.response) {
               console.error("Error submitting form:", error.response.data.errors);
               this.errors = error.response.data.errors;
-              console.log(this.errors, "error");
             }
           });
       },
--- resources/js/Components/Account Manager/Tracker/AmTrackerCreate.vue
+++ resources/js/Components/Account Manager/Tracker/AmTrackerCreate.vue
@@ -1,232 +1,238 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <table class="input_form">
-      <tr>
-        <td><label>Select Location</label></td>
-        <td>
-          <select
-            id="location"
-            v-model="tracker.selectedLocation"
-            @change="fetchClients"
-            @blur="checkValidation('selectedLocation')"
-            name="location"
-          >
-            <option value="">Select Location</option>
-            <option v-for="item in uniqueLocations" :key="item" :value="item">
-              {{ item }}
-            </option></select
-          ><br />
-          <span v-if="errors.selectedLocation" class="error">{{
-            errors.selectedLocation[0]
-          }}</span>
-        </td>
-      </tr>
+    <form @submit.prevent="submitForm">
+      <table class="input_form">
+        <tr>
+          <td><label>Select Location</label></td>
+          <td>
+            <select
+              id="location"
+              v-model="tracker.selectedLocation"
+              @change="fetchClients"
+              @blur="checkValidation('selectedLocation')"
+              name="location"
+            >
+              <option value="">Select Location</option>
+              <option v-for="item in uniqueLocations" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
+          </td>
+        </tr>
 
-      <tr v-if="tracker.selectedLocation">
-        <td><label>Client Name</label></td>
-        <td>
-          <select
-            id="client"
-            v-model="tracker.selectedClient"
-            @change="fetchBusinessUnits"
-            @blur="checkValidation('selectedClient')"
-            name="client"
-          >
-            <option value="">Select Client</option>
-            <option v-for="item in uniqueClients" :key="item" :value="item">
-              {{ item }}
-            </option></select
-          ><br />
-          <span v-if="errors.selectedClient" class="error">{{
-            errors.selectedClient[0]
-          }}</span>
-        </td>
-      </tr>
+        <tr v-if="tracker.selectedLocation">
+          <td><label>Client Name</label></td>
+          <td>
+            <select
+              id="client"
+              v-model="tracker.selectedClient"
+              @change="fetchBusinessUnits"
+              @blur="checkValidation('selectedClient')"
+              name="client"
+            >
+              <option value="">Select Client</option>
+              <option v-for="item in uniqueClients" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedClient" class="error">{{ errors.selectedClient[0] }}</span>
+          </td>
+        </tr>
 
-      <tr v-if="tracker.selectedClient">
-        <td><label>Business Unit</label></td>
-        <td>
-          <select
-            id="business"
-            v-model="tracker.selectedBusiness"
-            @blur="checkValidation('selectedBusiness')"
-            name="business"
-          >
-            <option value="">Select Unit</option>
-            <option v-for="item in uniqueBusinessUnits" :key="item" :value="item">
-              {{ item }}
-            </option></select
-          ><br />
-          <span v-if="errors.selectedBusiness" class="error">{{
-            errors.selectedBusiness[0]
-          }}</span>
-        </td>
-      </tr>
+        <tr v-if="tracker.selectedClient">
+          <td><label>Business Unit</label></td>
+          <td>
+            <select
+              id="business"
+              v-model="tracker.selectedBusiness"
+              @change="fetchClientManagerName"
+              @blur="checkValidation('selectedBusiness')"
+              name="business"
+            >
+              <option value="">Select Unit</option>
+              <option v-for="item in uniqueBusinessUnits" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedBusiness" class="error">{{ errors.selectedBusiness[0] }}</span>
+          </td>
+        </tr>
 
-      <tr v-if="tracker.selectedClient">
-        <td><label>Client Manager Name</label></td>
-        <td>
-          <input
-            type="text"
-            v-model="tracker.clientManagerName"
-            @blur="checkValidation('clientManagerName')"
-            placeholder="Client Manager Name"
-          /><br />
-          <span v-if="errors.clientManagerName" class="error">{{
-            errors.clientManagerName[0]
-          }}</span>
-        </td>
-      </tr>
+        <tr v-if="tracker.selectedBusiness">
+          <td><label>Client Manager Name</label></td>
+          <td>
+            <input
+              type="text"
+              v-model="tracker.clientManagerName"
+              @change="enableUploadFile"
+              @blur="checkValidation('clientManagerName')"
+              placeholder="Client Manager Name"
+            />
+            <br />
+            <span v-if="errors.clientManagerName" class="error">{{ errors.clientManagerName[0] }}</span>
+          </td>
+        </tr>
 
-      <tr>
-        <td><label>Upload File</label></td>
-        <td>
-          <input
-            type="file"
-            @change="handleFileChange"
-            accept=".xls, .xlsx"
-            name="file"
-            placeholder="Upload"
-            @blur="checkValidation('file')"
-          /><br />
-          <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
-        </td>
-      </tr>
+        <tr v-if="tracker.clientManagerName">
+          <td><label>Upload File</label></td>
+          <td>
+            <input
+              type="file"
+              @change="handleFileChange"
+              accept=".xls, .xlsx"
+              name="file"
+              placeholder="Upload"
+              @blur="checkValidation('file')"
+            />
+            <br />
+            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+          </td>
+        </tr>
 
-      <tr>
-        <td></td>
-        <td>
-          <button @click="closePopup()" class="cancel_btn">Cancel</button>
-          <button type="submit" class="submit_btn">Submit</button>
-        </td>
-      </tr>
-    </table>
-  </form>
-</template>
+        <tr>
+          <td></td>
+          <td>
+            <button @click="closePopup()" class="cancel_btn">Cancel</button>
+            <button type="submit" class="submit_btn">Submit</button>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </template>
 
-<script>
-import { commonFunctionsMixin } from "../../../function.js";
-import Swal from "sweetalert2";
-import axios from "axios";
+  <script>
+  import { commonFunctionsMixin } from "../../../function.js";
+  import Swal from "sweetalert2";
+  import axios from "axios";
 
-export default {
-  name: "AmTrackerCreate",
-  mixins: [commonFunctionsMixin],
-  data() {
-    return {
-      tracker: {
-        selectedClient: "",
-        clientManagerName: "",
-        selectedBusiness: "",
-        selectedLocation: "",
-        file: null,
+  export default {
+    name: "AmTrackerCreate",
+    mixins: [commonFunctionsMixin],
+    data() {
+      return {
+        tracker: {
+          selectedClient: "",
+          clientManagerName: "",
+          selectedBusiness: "",
+          selectedLocation: "",
+          file: null,
+        },
+        submitted: false,
+        clientData: [],
+        errors: {},
+      };
+    },
+    computed: {
+      uniqueLocations() {
+        return [...new Set(this.clientData.map((item) => item.location))];
       },
-      submitted: false,
-      clientData: [],
-      errors: {},
-    };
-  },
-  computed: {
-    uniqueLocations() {
-      return [...new Set(this.clientData.map((item) => item.location))];
+      uniqueClients() {
+        return [
+          ...new Set(
+            this.clientData
+              .filter((item) => item.location === this.tracker.selectedLocation)
+              .map((item) => item.client_name)
+          ),
+        ];
+      },
+      uniqueBusinessUnits() {
+        return [
+          ...new Set(
+            this.clientData
+              .filter((item) => item.client_name === this.tracker.selectedClient)
+              .map((item) => item.business_unit_name)
+          ),
+        ];
+      },
     },
-    uniqueClients() {
-      return [
-        ...new Set(
-          this.clientData
-            .filter((item) => item.location === this.tracker.selectedLocation)
-            .map((item) => item.client_name)
-        ),
-      ];
-    },
-    uniqueBusinessUnits() {
-      return [
-        ...new Set(
-          this.clientData
-            .filter((item) => item.client_name === this.tracker.selectedClient)
-            .map((item) => item.business_unit_name)
-        ),
-      ];
-    },
-  },
-  methods: {
-    closePopup() {
-      this.$emit("closePopup");
-    },
-    handleFileChange(event) {
-      this.tracker.file = event.target.files[0];
-    },
-    userLocationApi() {
-      axios
-        .get("/api/amtracker-create")
-        .then((response) => {
-          this.clientData = response.data.client;
-          console.log(this.clientData, "client");
-        })
-        .catch((error) => {
-          console.log(error);
-          this.errored = true;
-        });
-    },
-    resetForm() {
-      this.tracker.selectedClient = "";
-      this.tracker.clientManagerName = "";
-      this.tracker.selectedBusiness = "";
-      this.tracker.selectedLocation = "";
-      this.tracker.file = null;
-    },
-    checkValidation(fieldName) {
-      if (this.errors.hasOwnProperty(fieldName)) {
-        delete this.errors[fieldName];
-      }
-    },
-    fetchClients() {
-      this.tracker.selectedClient = "";
-      this.tracker.selectedBusiness = "";
-      this.tracker.clientManagerName = "";
-    },
-    fetchBusinessUnits() {
-      this.tracker.selectedBusiness = "";
-    },
-    submitForm() {
-      this.submitted = true; // Set the submitted flag to true when attempting to submit the form
-      let formData = new FormData();
-      formData.append("selectedClient", this.tracker.selectedClient);
-      formData.append("clientManagerName", this.tracker.clientManagerName);
-      formData.append("selectedBusiness", this.tracker.selectedBusiness);
-      formData.append("selectedLocation", this.tracker.selectedLocation);
-      formData.append("file", this.tracker.file);
+    methods: {
+      closePopup() {
+        this.$emit("closePopup");
+      },
+      handleFileChange(event) {
+        this.tracker.file = event.target.files[0];
+      },
+      userLocationApi() {
+        axios
+          .get("/api/amtracker-create")
+          .then((response) => {
+            this.clientData = response.data.client;
+            console.log(this.clientData, "client");
+          })
+          .catch((error) => {
+            console.log(error);
+            this.errored = true;
+          });
+      },
+      resetForm() {
+        this.tracker.selectedClient = "";
+        this.tracker.clientManagerName = "";
+        this.tracker.selectedBusiness = "";
+        this.tracker.selectedLocation = "";
+        this.tracker.file = null;
+      },
+      checkValidation(fieldName) {
+        if (this.errors.hasOwnProperty(fieldName)) {
+          delete this.errors[fieldName];
+        }
+      },
+      fetchClients() {
+        this.tracker.selectedClient = "";
+        this.tracker.selectedBusiness = "";
+        this.tracker.clientManagerName = "";
+      },
+      fetchBusinessUnits() {
+        this.tracker.selectedBusiness = "";
+        this.tracker.clientManagerName = "";
+      },
+      fetchClientManagerName() {
+        this.tracker.clientManagerName = "";
+      },
+      enableUploadFile() {
+        this.tracker.file = null;
+      },
+      submitForm() {
+        this.submitted = true; // Set the submitted flag to true when attempting to submit the form
+        let formData = new FormData();
+        formData.append("selectedClient", this.tracker.selectedClient);
+        formData.append("clientManagerName", this.tracker.clientManagerName);
+        formData.append("selectedBusiness", this.tracker.selectedBusiness);
+        formData.append("selectedLocation", this.tracker.selectedLocation);
+        formData.append("file", this.tracker.file);
 
-      axios
-        .post("/api/amtracker-create", formData)
-        .then((response) => {
-          console.log("Form submitted:", response.data.message);
-          if (response.data.message) {
-            this.errors = {};
+        axios
+          .post("/api/amtracker-create", formData)
+          .then((response) => {
+            console.log("Form submitted:", response.data.message);
+            if (response.data.message) {
+              this.errors = {};
 
-            Swal.fire({
-              position: "top-center",
-              icon: "success",
-              title: "Tracker created successfully",
-              showConfirmButton: false,
-              timer: 3000,
-            });
-            this.resetForm();
-          } else {
-            Swal.fire("Form not Submitted");
-          }
+              Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: "Tracker created successfully",
+                showConfirmButton: false,
+                timer: 3000,
+              });
+              this.resetForm();
+            } else {
+              Swal.fire("Form not Submitted");
+            }
 
-          // Handle the response as needed
-        })
-        .catch((error) => {
-          this.errors = error.response.data.errors;
-        });
+            // Handle the response as needed
+          })
+          .catch((error) => {
+            this.errors = error.response.data.errors;
+          });
 
-      // You might want to reset the form and submitted flag here if needed
+        // You might want to reset the form and submitted flag here if needed
+      },
     },
-  },
-  mounted() {
-    this.userLocationApi();
-  },
-};
-</script>
+    mounted() {
+      this.userLocationApi();
+    },
+  };
+  </script>
