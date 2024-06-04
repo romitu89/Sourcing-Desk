145f1521500d589feb8566a01cab3a7c9993b8e5
sreejithnath90 <sreejithnath90@gmail.com>
2024-06-04 18:56:05 +0530

    SO-DE-129 : Fixed the issue with api for client and location

--- app/Http/Controllers/AccountManager/TeamManagement/TeamCreate.php
+++ app/Http/Controllers/AccountManager/TeamManagement/TeamCreate.php
@@ -12,13 +12,13 @@ class TeamCreate extends Controller
 {
     public function create()
     {
-        $loc = Client::select('location')->distinct()->get();
-        $client = Client::select('client_name')->distinct()->where("account_manager_id", auth()->user()->id)->get();
+        // $loc = Client::select('location')->distinct()->get();
+        $client = Client::select('location','client_name')->distinct()->where("account_manager_id", auth()->user()->id)->get();
         $tm = User::select('email_id')->distinct()->where('reporting_to_am', auth()->user()->email_id)->get();
 
         return response()->json([
             'teamEmail' => $tm,
-            'location' => $loc,
+            // 'location' => $loc,
             'client' => $client
         ]);
     }
