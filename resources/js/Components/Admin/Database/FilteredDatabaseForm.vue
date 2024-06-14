6178700595cccb76cc93620c320ff864926a3bec
romit <romitu89@gmail.com>
2024-06-14 19:30:11 +0530

    admin done

--- resources/js/Components/Admin/Database/FilteredDatabaseForm.vue
+++ resources/js/Components/Admin/Database/FilteredDatabaseForm.vue
@@ -1,111 +1,134 @@
 <template>
-   <form @submit.prevent="submitForm">
-    <table class="input_form">
+    <form @submit.prevent="submitForm">
+      <table class="input_form">
 
-    <tr>
-    <td ><label >Select Country</label></td>
-    <td ><select id="location"  v-model="client.selectedLocation" @blur="checkValidation('selectedLocation')"  name="Choose location">
-    <option value="">Select Country</option>
-    <option
-              v-for="item in userData"
-              :key="item.country"
-              :value="item.country"
+        <!-- Select Country Field -->
+        <tr>
+          <td><label>Select Country</label></td>
+          <td>
+            <select
+              id="location"
+              v-model="client.selectedLocation"
+              @change="handleCountryChange"
+              @blur="checkValidation('selectedLocation')"
+              name="Choose location"
             >
-              {{ item.country }}
-            </option></select
-          ><br /><span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
-        </td>
-      </tr>
+              <option value="">Select Country</option>
+              <option
+                v-for="item in userData"
+                :key="item.country"
+                :value="item.country"
+              >
+                {{ item.country }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
+          </td>
+        </tr>
 
+        <!-- Select Duration Field (Conditional Rendering) -->
+        <tr v-if="client.selectedLocation">
+          <td><label>Select Duration</label></td>
+          <td>
+            <select
+              id="duration"
+              v-model="client.SelectedDuration"
+              @blur="checkValidation('SelectedDuration')"
+              name="Choose duration"
+            >
+              <option value="">Select Duration</option>
+              <option value="7">7 Days</option>
+              <option value="15">15 Days</option>
+              <option value="3">3 Months</option>
+              <option value="6">6 Months</option>
+              <option value="One">One Year</option>
+              <option value="All">All</option>
+            </select>
+            <br />
+            <span v-if="errors.SelectedDuration" class="error">{{ errors.SelectedDuration[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-    <td ><label >Select Duration</label></td>
-    <td ><select id="duration"  v-model="client.SelectedDuration"  @blur="checkValidation('SelectedDuration')"  name="Choose duration">
-    <option value="">Select Duration</option>
-    <option value="7">7 Days</option>
-    <option value="15">15 Days</option>
-    <option value="3">3 Months</option>
-    <option value="6">6 Months</option>
-    <option value="One">One Year</option>
-    <option value="All">All</option>
-    </select><br>
-    <span v-if="errors.SelectedDuration" class="error">{{ errors.SelectedDuration[0] }}</span></td>
-    </tr>
+        <!-- Form Buttons -->
+        <tr>
+          <td></td>
+          <td>
+            <button @click="closePopup()" class="cancel_btn">Cancel</button>
+            <button class="submit_btn">Submit</button>
+          </td>
+        </tr>
 
-    <tr>
-        <td></td>
-       <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-        <button class="submit_btn">Submit</button> </td>
-    </tr>
-</table>
-</form>
-</template>
+      </table>
+    </form>
+  </template>
 
-<script>
-import { commonFunctionsMixin } from '../../../function.js';
-import Swal from 'sweetalert2'
-    export default {
-        name:'FilteredDatabaseForm',
-        mixins:[commonFunctionsMixin],
-        data()
-       {
-           return{
-               client: {
-                selectedLocation:"",
-                SelectedDuration:"",
+  <script>
+  import Swal from 'sweetalert2';
+  import axios from 'axios';
+  import { commonFunctionsMixin } from '../../../function.js';
 
-
-               },
-               submitted:false,
-               userData:[],
-                errors:{},
-           };
-       },
-
-
-           methods:{
-            closePopup() {
-      this.$emit("closePopup");
+  export default {
+    name: 'FilteredDatabaseForm',
+    mixins: [commonFunctionsMixin],
+    data() {
+      return {
+        client: {
+          selectedLocation: "",
+          SelectedDuration: "",
+        },
+        submitted: false,
+        userData: [],
+        errors: {},
+      };
     },
-   
-    userLocationApi() {
-      axios
-        .get("/api/adminDatabase-filteredData")
-        .then((response) => {
-          this.userData = response.data.location;
-          console.log(this.userData, "userData");
-        })
-        .catch((error) => {
-          console.log(error);
-          this.errored = true;
-        });
+    methods: {
+      closePopup() {
+        this.$emit("closePopup");
+      },
+      userLocationApi() {
+        axios
+          .get("/api/adminDatabase-filteredData")
+          .then((response) => {
+            this.userData = response.data.location;
+            console.log(this.userData, "userData");
+          })
+          .catch((error) => {
+            console.log(error);
+            this.errored = true;
+          });
+      },
+      submitForm() {
+        this.submitted = true;
+        axios
+          .post("/api/adminDatabase-filteredData", this.client)
+          .then((response) => {
+            this.errors = {};
+            console.log("Form submitted:", response.data.results);
+            if (Object.values(this.errors).length === 0) {
+              this.buttonAction = true;
+            }
+            this.results = response.data.results;
+            // Handle the response as needed
+          })
+          .catch((error) => {
+            console.error("Error submitting form:", error.response.data.errors);
+            this.errors = error.response.data.errors;
+          });
+      },
+      checkValidation(fieldName) {
+        if (this.errors.hasOwnProperty(fieldName)) {
+          delete this.errors[fieldName];
+        }
+      },
+      handleCountryChange() {
+        // Reset fields when country changes
+        this.client.SelectedDuration = "";
+        this.errors = {};
+      },
     },
-
-    submitForm() {
-      this.submitted = true;
-
-
-      axios
-        .post("/api/adminDatabase-filteredData", this.client)
-        .then((response) => {
-          this.errors = {};
-
-          console.log("Form submitted:", response.data.results);
-          if (Object.values(this.errors).length == 0) {
-            this.buttonAction = true;
-          }
-          this.results = response.data.results;
-
-          // Handle the response as needed
-        })
-        .catch((error) => {
-          console.error("Error submitting form:", error.response.data.errors);
-          this.errors = error.response.data.errors;
-        });
+    mounted() {
+      this.userLocationApi();
     },
-  },
-  mounted() {
-    this.userLocationApi();
-  },
-};
-</script>
+  };
+  </script>
--- resources/js/Components/Admin/Database/WorkingCandidateForm.vue
+++ resources/js/Components/Admin/Database/WorkingCandidateForm.vue
@@ -1,109 +1,157 @@
 <template>
-   <form @submit.prevent="submitForm">
-    <table class="input_form">
+    <form @submit.prevent="submitForm">
+      <table class="input_form">
 
-    <tr>
-    <td ><label >Select Country</label></td>
-    <td ><select id="location" v-model="client.selectedLocation" @blur="checkValidation('selectedLocation')" name="Choose Location">
-    <option value="">Select Country</option>
-    <option
-              v-for="item in userData"
-              :key="item.country"
-              :value="item.country"
+        <!-- Select Country Field -->
+        <tr>
+          <td><label>Select Country</label></td>
+          <td>
+            <select
+              id="location"
+              v-model="client.selectedLocation"
+              @change="showFromDateField"
+              @blur="checkValidation('selectedLocation')"
+              name="Choose Location"
             >
-              {{ item.country }}
-            </option></select
-          ><br /><span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
-        </td>
-      </tr>
+              <option value="">Select Country</option>
+              <option
+                v-for="item in userData"
+                :key="item.country"
+                :value="item.country"
+              >
+                {{ item.country }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-   <td ><label >From Date</label></td>
-   <td ><input  v-model.trim="client.fromDate" @blur="checkValidation('fromDate')" type="date" >
-    <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span></td>
-   </tr>
+        <!-- From Date Field -->
+        <tr v-if="client.selectedLocation">
+          <td><label>From Date</label></td>
+          <td>
+            <input
+              v-model.trim="client.fromDate"
+              @blur="checkValidation('fromDate')"
+              type="date"
+            />
+            <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span>
+          </td>
+        </tr>
 
-   <tr>
-   <td ><label >To Date</label></td>
-   <td ><input  v-model.trim="client.toDate" @blur="checkValidation('toDate')" type="date" >
-    <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span></td>
-   </tr>
+        <!-- To Date Field -->
+        <tr v-if="client.fromDate">
+          <td><label>To Date</label></td>
+          <td>
+            <input
+              v-model.trim="client.toDate"
+              @blur="checkValidation('toDate')"
+              type="date"
+            />
+            <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span>
+          </td>
+        </tr>
 
+        <!-- Form Buttons -->
+        <tr>
+          <td></td>
+          <td>
+            <button @click="closePopup()" class="cancel_btn">Cancel</button>
+            <button class="submit_btn">Submit</button>
+          </td>
+        </tr>
 
-   <tr>
-        <td></td>
-       <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-        <button class="submit_btn">Submit</button> </td>
-    </tr>
-</table>
-</form>
-</template>
+      </table>
+    </form>
+  </template>
 
-<script>
-import Swal from 'sweetalert2'
-    export default {
-        name:'WorkingCandidateForm',
+  <script>
+  import Swal from 'sweetalert2';
+  import axios from 'axios';
 
+  export default {
+    name: 'WorkingCandidateForm',
 
-        data()
-       {
-           return{
-               client: {
-                selectedLocation:"",
-                fromDate:"",
-                toDate:"",
-
-               },
-               submitted:false,
-               userData:[],
-                errors:{},
-           };
-       },
-
-
-           methods:{
-            closePopup() {
-      this.$emit("closePopup");
+    data() {
+      return {
+        client: {
+          selectedLocation: "",
+          fromDate: "",
+          toDate: "",
+        },
+        submitted: false,
+        userData: [],
+        errors: {},
+        showFromDate: false,
+        showToDate: false,
+      };
     },
 
-    userLocationApi() {
-      axios
-        .get("/api/adminDatabase-workingcandidate")
-        .then((response) => {
-          this.userData = response.data.location;
-          console.log(this.userData, "userData");
-        })
-        .catch((error) => {
-          console.log(error);
-          this.errored = true;
-        });
+    methods: {
+      closePopup() {
+        this.$emit("closePopup");
+      },
+
+      userLocationApi() {
+        axios
+          .get("/api/adminDatabase-workingcandidate")
+          .then((response) => {
+            this.userData = response.data.location;
+            console.log(this.userData, "userData");
+          })
+          .catch((error) => {
+            console.log(error);
+            this.errored = true;
+          });
+      },
+
+      submitForm() {
+        this.submitted = true;
+
+        axios
+          .post("/api/adminDatabase-workingcandidate", this.client)
+          .then((response) => {
+            this.errors = {};
+
+            console.log("Form submitted:", response.data.results);
+            if (Object.values(this.errors).length === 0) {
+              this.buttonAction = true;
+            }
+            this.results = response.data.results;
+
+            // Handle the response as needed
+            Swal.fire({
+              position: "top-center",
+              icon: "success",
+              title: "Form submitted successfully",
+              showConfirmButton: false,
+              timer: 3000,
+            });
+          })
+          .catch((error) => {
+            console.error("Error submitting form:", error.response.data.errors);
+            this.errors = error.response.data.errors;
+          });
+      },
+
+      checkValidation(fieldName) {
+        if (this.errors.hasOwnProperty(fieldName)) {
+          delete this.errors[fieldName];
+        }
+      },
+
+      showFromDateField() {
+        this.showFromDate = true;
+      },
+
+      showToDateField() {
+        this.showToDate = true;
+      },
     },
 
-    submitForm() {
-      this.submitted = true;
-
-
-      axios
-        .post("/api/adminDatabase-workingcandidate", this.client)
-        .then((response) => {
-          this.errors = {};
-
-          console.log("Form submitted:", response.data.results);
-          if (Object.values(this.errors).length == 0) {
-            this.buttonAction = true;
-          }
-          this.results = response.data.results;
-
-          // Handle the response as needed
-        })
-        .catch((error) => {
-          console.error("Error submitting form:", error.response.data.errors);
-          this.errors = error.response.data.errors;
-        });
+    mounted() {
+      this.userLocationApi();
     },
-  },
-  mounted() {
-    this.userLocationApi();
-  },
-};
-</script>
+  };
+  </script>
