b106e0c70db7e183c65fb0df36c47db54c0e8f72
romit <romitu89@gmail.com>
2024-06-14 19:05:54 +0530

    SO-DE-131: User Mis logic done

--- resources/js/Components/Admin/User Mis/UserAccountManagerForm.vue
+++ resources/js/Components/Admin/User Mis/UserAccountManagerForm.vue
@@ -1,105 +1,141 @@
 <template>
-     <form @submit.prevent="submitForm">
-    <table class="input_form">
-
-    <tr>
-    <td ><label >Employee Name</label></td>
-    <td ><select id="empname"  v-model="client.employeeName" @blur="checkValidation('employeeName')" name="empname">
-    <option value="">Select Employee Name</option>
-    <option
-              v-for="item in userData"
-              :key="item.employee_name"
-              :value="item.employee_name"
+    <form @submit.prevent="submitForm">
+      <table class="input_form">
+        <tr>
+          <td><label>Employee Name</label></td>
+          <td>
+            <select
+              id="empname"
+              v-model="client.employeeName"
+              @change="filterEmailData"
+              @blur="checkValidation('employeeName')"
+              name="empname"
             >
-              {{ item.employee_name }}
-            </option></select
-          ><br /><span v-if="errors.employeeName" class="error">{{ errors.employeeName[0] }}</span>
-        </td>
-      </tr>
+              <option value="">Select Employee Name</option>
+              <option v-for="item in uniqueEmployeeNames" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.employeeName" class="error">{{ errors.employeeName[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-    <td ><label >Employee Email Id</label></td>
-    <td ><select id="email"  v-model="client.employeeEmail" @blur="checkValidation('employeeEmail')" name="Choose Client">
-    <option value="">Select Email</option>
-    <option
-              v-for="item in userData"
-              :key="item.email_id"
-              :value="item.email_id"
+        <tr v-if="client.employeeName">
+          <td><label>Employee Email Id</label></td>
+          <td>
+            <select
+              id="email"
+              v-model="client.employeeEmail"
+              @blur="checkValidation('employeeEmail')"
+              name="Choose Client"
             >
-              {{ item.email_id }}
-            </option></select
-          ><br /><span v-if="errors.employeeEmail" class="error">{{ errors.employeeEmail[0] }}</span>
-        </td>
-      </tr>
+              <option value="">Select Email</option>
+              <option v-for="item in filteredEmails" :key="item.email_id" :value="item.email_id">
+                {{ item.email_id }}
+              </option>
+            </select>
+            <br />
+            <span v-if="errors.employeeEmail" class="error">{{ errors.employeeEmail[0] }}</span>
+          </td>
+        </tr>
 
+        <tr v-if="client.employeeEmail">
+          <td><label>Select Matrix</label></td>
+          <td>
+            <select
+              id="matrix"
+              v-model="client.selectedMatrix"
+              @blur="checkValidation('selectedMatrix')"
+              name="matrix"
+            >
+              <option value="">Select Matrix</option>
+              <option value="Requirements">Requirements</option>
+              <option value="Submission">Submission</option>
+              <option value="Selection">Selection</option>
+              <option value="Rejections">Rejections</option>
+              <option value="Onboarded">Onboarded</option>
+              <option value="Dropouts">Dropouts</option>
+              <option value="Offboarded">Offboarded</option>
+              <option value="Daily Report">Daily Report</option>
+            </select>
+            <br />
+            <span v-if="errors.selectedMatrix" class="error">{{ errors.selectedMatrix[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-    <td ><label >Select Matrix</label></td>
-    <td ><select id="matrix"  v-model="client.selectedMatrix" @blur="checkValidation('selectedMatrix')" name="matrix">
-    <option value="">Select Matrix</option>
-    <option value="Requirements">Requirements</option>
-    <option value="Submission">Submission</option>
-    <option value="Selection">Selection</option>
-    <option value="Rejections">Rejections</option>
-    <option value="Onboarded">Onboarded</option>
-    <option value="Dropouts">Dropouts</option>
-    <option value="Offboarded">Offboarded</option>
-    <option value="Daily Report">Daily Report</option>
-    </select><br>
-    <span v-if="errors.selectedMatrix" class="error">{{ errors.selectedMatrix[0] }}</span></td>
-    </tr>
+        <tr v-if="client.selectedMatrix">
+          <td><label>From Date</label></td>
+          <td>
+            <input v-model="client.fromDate" @blur="checkValidation('fromDate')" type="date" />
+            <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-    <td ><label >From Date</label></td>
-    <td ><input  v-model="client.fromDate" @blur="checkValidation('fromDate')" type="date" >
-        <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span></td>
-    </tr>
+        <tr v-if="client.fromDate">
+          <td><label>To Date</label></td>
+          <td>
+            <input v-model="client.toDate" @blur="checkValidation('toDate')" type="date" />
+            <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span>
+          </td>
+        </tr>
 
-    <tr>
-    <td ><label >To Date</label></td>
-    <td ><input  v-model="client.toDate" @blur="checkValidation('toDate')" type="date" >
-        <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span></td>
-    </tr>
-
-    <tr>
-        <td></td>
-       <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-        <button class="submit_btn">Submit</button> </td>
-    </tr>
-</table>
-</form>
-</template>
-
-<script>
+        <tr>
+          <td></td>
+          <td>
+            <button @click="closePopup()" class="cancel_btn">Cancel</button>
+            <button class="submit_btn">Submit</button>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </template>
+  <script>
 import { commonFunctionsMixin } from '../../../function.js';
-import Swal from 'sweetalert2'
-    export default {
-        name:'UserAccountManagerForm',
-        mixins:[commonFunctionsMixin],
-        data()
-        {
-            return{
-                client: {
-                    employeeName:"",
-                    employeeEmail:"",
-                    selectedMatrix:"",
-                    fromDate:"",
-                    toDate:"",
-                },
-                submitted:false,
-                userData:[],
-                errors:{},
-            };
-        },
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
-
-            methods:{
-                closePopup() {
+export default {
+  name: 'UserAccountManagerForm',
+  mixins: [commonFunctionsMixin],
+  data() {
+    return {
+      client: {
+        employeeName: "",
+        employeeEmail: "",
+        selectedMatrix: "",
+        fromDate: "",
+        toDate: "",
+      },
+      submitted: false,
+      userData: [],
+      filteredEmails: [],
+      errors: {},
+    };
+  },
+  computed: {
+    uniqueEmployeeNames() {
+      const employeeNames = this.userData.map(item => item.employee_name);
+      return [...new Set(employeeNames)];
+    },
+  },
+  watch: {
+    'client.employeeName'() {
+      this.resetFormFields('employeeName');
+      this.filterEmailData();
+    },
+    'client.employeeEmail'() {
+      this.resetFormFields('employeeEmail');
+    },
+    'client.selectedMatrix'() {
+      this.resetFormFields('selectedMatrix');
+    },
+  },
+  methods: {
+    closePopup() {
       this.$emit("closePopup");
     },
-
-
-        userLocationApi() {
+    userLocationApi() {
       axios
         .get("/api/adminuserMis-accountManager")
         .then((response) => {
@@ -111,10 +147,13 @@ import Swal from 'sweetalert2'
           this.errored = true;
         });
     },
-
+    filterEmailData() {
+      this.filteredEmails = this.userData.filter(
+        item => item.employee_name === this.client.employeeName
+      );
+    },
     submitForm() {
       this.submitted = true;
-
 
       axios
         .post("/api/adminuserMis-accountManager", this.client)
@@ -127,6 +166,14 @@ import Swal from 'sweetalert2'
           }
           this.results = response.data.results;
 
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Form submitted successfully",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+
           // Handle the response as needed
         })
         .catch((error) => {
@@ -134,6 +181,22 @@ import Swal from 'sweetalert2'
           this.errors = error.response.data.errors;
         });
     },
+    checkValidation(fieldName) {
+      if (this.errors.hasOwnProperty(fieldName)) {
+        delete this.errors[fieldName];
+      }
+    },
+    resetFormFields(level) {
+      const resetMap = {
+        employeeName: ['employeeEmail', 'selectedMatrix', 'fromDate', 'toDate'],
+        employeeEmail: ['selectedMatrix', 'fromDate', 'toDate'],
+        selectedMatrix: ['fromDate', 'toDate'],
+      };
+      resetMap[level].forEach(field => {
+        this.client[field] = "";
+      });
+      this.errors = {};
+    }
   },
   mounted() {
     this.userLocationApi();
--- resources/js/Components/Admin/User Mis/UserMatrixForm.vue
+++ resources/js/Components/Admin/User Mis/UserMatrixForm.vue
@@ -1,119 +1,157 @@
 <template>
     <form @submit.prevent="submitForm">
-    <table class="input_form">
-
-        <tr>
-   <td ><label >Employee Name</label></td>
-   <td ><select id="employee"  v-model="client.employeeName"  @blur="checkValidation('employeeName')" name="employee">
-   <option value="">Select Employee</option>
-   <option
-              v-for="item in userData"
-              :key="item.employee_name"
-              :value="item.employee_name"
-            >
-              {{ item.employee_name }}
-            </option></select
-          ><br /><span v-if="errors.employeeName" class="error">{{ errors.employeeName[0] }}</span>
-        </td>
-      </tr>
-
-   <tr>
-   <td ><label >Employee Email Id</label></td>
-   <td ><select id="email"  v-model.trim="client.employeeEmail"  @blur="checkValidation('employeeEmail')" name="Choose Client">
-   <option value="">Select Email</option>
-   <option
-              v-for="item in userData"
-              :key="item.email_id"
-              :value="item.email_id"
-            >
-              {{ item.email_id }}
-            </option></select
-          ><br /><span v-if="errors.employeeEmail" class="error">{{ errors.employeeEmail[0] }}</span>
-        </td>
-      </tr>
-
-    <tr>
-    <td ><label >Select Matrix</label></td>
-    <td ><select id="matrix"  v-model="client.selectedMatrix"  @blur="checkValidation('selectedMatrix')" name="matrix">
-    <option value="">Select Matrix</option>
-    <option value="Selections-Skill">Selections-Skill</option>
-                            <option value="Rejections-Skill">Rejections-Skill</option>
-    </select><br>
-    <span v-if="errors.selectedMatrix" class="error">{{ errors.selectedMatrix[0] }}</span></td>
-    </tr>
-
-    <tr>
-        <td></td>
-       <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-        <button class="submit_btn">Submit</button> </td>
-    </tr>
-</table>
-</form>
+        <table class="input_form">
+            <tr>
+                <td><label>Employee Name</label></td>
+                <td>
+                    <select id="employee" v-model="client.employeeName" @change="handleFieldChange('employeeName')" @blur="checkValidation('employeeName')" name="employee">
+                        <option value="">Select Employee</option>
+                        <option v-for="item in uniqueEmployeeNames" :key="item" :value="item">{{ item }}</option>
+                    </select>
+                    <br />
+                    <span v-if="errors.employeeName" class="error">{{ errors.employeeName[0] }}</span>
+                </td>
+            </tr>
+            <tr v-if="client.employeeName">
+                <td><label>Employee Email Id</label></td>
+                <td>
+                    <select id="email" v-model.trim="client.employeeEmail" @change="handleFieldChange('employeeEmail')" @blur="checkValidation('employeeEmail')" name="Choose Client">
+                        <option value="">Select Email</option>
+                        <option v-for="item in filteredEmails" :key="item.email_id" :value="item.email_id">{{ item.email_id }}</option>
+                    </select>
+                    <br />
+                    <span v-if="errors.employeeEmail" class="error">{{ errors.employeeEmail[0] }}</span>
+                </td>
+            </tr>
+            <tr v-if="client.employeeEmail">
+                <td><label>Select Matrix</label></td>
+                <td>
+                    <select id="matrix" v-model="client.selectedMatrix" @change="handleFieldChange('selectedMatrix')" @blur="checkValidation('selectedMatrix')" name="matrix">
+                        <option value="">Select Matrix</option>
+                        <option value="Selections-Skill">Selections-Skill</option>
+                        <option value="Rejections-Skill">Rejections-Skill</option>
+                    </select>
+                    <br />
+                    <span v-if="errors.selectedMatrix" class="error">{{ errors.selectedMatrix[0] }}</span>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button @click="closePopup()" class="cancel_btn">Cancel</button>
+                    <button class="submit_btn">Submit</button>
+                </td>
+            </tr>
+        </table>
+    </form>
 </template>
-
 <script>
 import { commonFunctionsMixin } from '../../../function.js';
-import Swal from 'sweetalert2'
-    export default {
-        name:'UserMatrixForm',
-        mixins:[commonFunctionsMixin],
-        data()
-       {
-           return{
-               client: {
-                employeeName:"",
-                employeeEmail:"",
-                selectedMatrix:"",
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
-               },
-               submitted:false,
-               userData:[],
-                errors:{},
-           };
-       },
-
-
-           methods:{
-            closePopup() {
-      this.$emit("closePopup");
+export default {
+    name: 'UserMatrixForm',
+    mixins: [commonFunctionsMixin],
+    data() {
+        return {
+            client: {
+                employeeName: "",
+                employeeEmail: "",
+                selectedMatrix: "",
+            },
+            submitted: false,
+            userData: [],
+            filteredEmails: [],
+            errors: {},
+        };
     },
-
-    userLocationApi() {
-      axios
-        .get("/api/adminuserMis-userMatrix")
-        .then((response) => {
-          this.userData = response.data.User;
-          console.log(this.userData, "userData");
-        })
-        .catch((error) => {
-          console.log(error);
-          this.errored = true;
-        });
+    computed: {
+        uniqueEmployeeNames() {
+            const employeeNames = this.userData.map(item => item.employee_name);
+            return [...new Set(employeeNames)];
+        },
     },
-
-    submitForm() {
-      this.submitted = true;
-      axios
-        .post("/api/adminuserMis-userMatrix", this.client)
-        .then((response) => {
-          this.errors = {};
-
-          console.log("Form submitted:", response.data.results);
-          if (Object.values(this.errors).length == 0) {
-            this.buttonAction = true;
-          }
-          this.results = response.data.results;
-
-          // Handle the response as needed
-        })
-        .catch((error) => {
-          console.error("Error submitting form:", error.response.data.errors);
-          this.errors = error.response.data.errors;
-        });
+    watch: {
+        'client.employeeName'() {
+            this.resetDependentFields('employeeName');
+            this.filterEmailData();
+        },
+        'client.employeeEmail'() {
+            this.resetDependentFields('employeeEmail');
+        },
+        'client.selectedMatrix'() {
+            this.resetDependentFields('selectedMatrix');
+        },
     },
-  },
-  mounted() {
-    this.userLocationApi();
-  },
+    methods: {
+        closePopup() {
+            this.$emit("closePopup");
+        },
+        userLocationApi() {
+            axios
+                .get("/api/adminuserMis-userMatrix")
+                .then((response) => {
+                    this.userData = response.data.User;
+                    console.log(this.userData, "userData");
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.errored = true;
+                });
+        },
+        filterEmailData() {
+            this.filteredEmails = this.userData.filter(
+                item => item.employee_name === this.client.employeeName
+            );
+        },
+        submitForm() {
+            this.submitted = true;
+            axios
+                .post("/api/adminuserMis-userMatrix", this.client)
+                .then((response) => {
+                    this.errors = {};
+
+                    console.log("Form submitted:", response.data.results);
+                    if (Object.values(this.errors).length === 0) {
+                        this.buttonAction = true;
+                    }
+                    this.results = response.data.results;
+                    Swal.fire({
+                        position: "top-center",
+                        icon: "success",
+                        title: "Form submitted successfully",
+                        showConfirmButton: false,
+                        timer: 3000,
+                    });
+                })
+                .catch((error) => {
+                    console.error("Error submitting form:", error.response.data.errors);
+                    this.errors = error.response.data.errors;
+                });
+        },
+        checkValidation(fieldName) {
+            if (this.errors.hasOwnProperty(fieldName)) {
+                delete this.errors[fieldName];
+            }
+        },
+        resetDependentFields(level) {
+            const resetMap = {
+                employeeName: ['employeeEmail', 'selectedMatrix'],
+                employeeEmail: ['selectedMatrix'],
+                selectedMatrix: [],
+            };
+            resetMap[level].forEach(field => {
+                this.client[field] = "";
+            });
+            this.errors = {};
+        },
+        handleFieldChange(level) {
+            this.resetDependentFields(level);
+        }
+    },
+    mounted() {
+        this.userLocationApi();
+    },
 };
 </script>
