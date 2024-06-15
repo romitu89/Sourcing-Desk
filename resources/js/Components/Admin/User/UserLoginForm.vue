9ac14cf399b3745716459a48e1b28f84139b86fa
romit <romitu89@gmail.com>
2024-06-15 13:10:08 +0530

    work in progress with logic

--- resources/js/Components/Admin/User/UserLoginForm.vue
+++ resources/js/Components/Admin/User/UserLoginForm.vue
@@ -4,35 +4,35 @@
         <tr>
           <td><label>Select Location</label></td>
           <td>
-            <select id="location" v-model="employee.selectedLocation" @blur="checkValidation('selectedLocation')" name="location">
+            <select id="location" v-model="employee.selectedLocation" @change="updateFilteredData" name="location">
               <option value="">Select Location</option>
-              <option v-for="item in getUniqueValues(loginData,'location')" :key='item' :value="item">{{ item }}</option>
+              <option v-for="item in getUniqueValues(loginData, 'location')" :key='item' :value="item">{{ item }}</option>
             </select><br>
-            <span v-if="errors.selectedLocation" class="error">{{errors.selectedLocation[0]}}</span>
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
           </td>
         </tr>
-        <tr>
+        <tr v-if="employee.selectedLocation">
           <td><label>Employee Email</label></td>
           <td>
-            <select id="email" v-model="employee.selectedEmail" @blur="checkValidation('selectedEmail')" name="email">
+            <select id="email" v-model="employee.selectedEmail" @change="resetFormFields('email')" name="email">
               <option value="">Select Email</option>
-              <option v-for="item in getUniqueValues(loginData,'email_id')" :key='item' :value="item">{{ item }}</option>
+              <option v-for="item in filteredEmails" :key='item' :value="item">{{ item }}</option>
             </select><br>
-            <span v-if="errors.selectedEmail" class="error">{{errors.selectedEmail[0]}}</span>
+            <span v-if="errors.selectedEmail" class="error">{{ errors.selectedEmail[0] }}</span>
           </td>
         </tr>
-        <tr>
+        <tr v-if="employee.selectedEmail">
           <td><label>From Date</label></td>
           <td>
-            <input type="date" v-model="employee.fromDate" @blur="checkValidation('fromDate')">
-            <span v-if="errors.fromDate" class="error">{{errors.fromDate[0]}}</span>
+            <input type="date" v-model="employee.fromDate" @change="resetToDate">
+            <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span>
           </td>
         </tr>
-        <tr>
+        <tr v-if="employee.fromDate">
           <td><label>To Date</label></td>
           <td>
-            <input type="date" v-model="employee.toDate" @blur="checkValidation('toDate')" >
-            <span v-if="errors.toDate" class="error">{{errors.toDate[0]}}</span>
+            <input type="date" v-model="employee.toDate" :min="employee.fromDate">
+            <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span>
           </td>
         </tr>
         <tr>
@@ -78,6 +78,7 @@
           { label: 'Created At', key: 'formatted_created_at' },
           { label: 'Logout Time', key: 'formatted_logout_time' },
         ],
+        filteredEmails: [],
       };
     },
     computed: {
@@ -93,6 +94,34 @@
       closePopup() {
         this.$emit("closePopup");
       },
+
+      resetFormFields(field) {
+        switch (field) {
+          case 'location':
+            this.employee.selectedEmail = "";
+            this.employee.fromDate = "";
+            this.employee.toDate = "";
+            break;
+          case 'email':
+            this.employee.fromDate = "";
+            this.employee.toDate = "";
+            break;
+        }
+      },
+
+      resetToDate() {
+        this.employee.toDate = "";
+      },
+
+      updateFilteredData() {
+        this.filteredEmails = this.getUniqueValues(this.loginData.filter(item => item.location === this.employee.selectedLocation), 'email_id');
+        this.resetFormFields('location');
+      },
+
+      getUniqueValues(data, key) {
+        return [...new Set(data.map(item => item[key]))];
+      },
+
       userLocationApi() {
         axios
           .get('/api/adminuser-login')
@@ -104,6 +133,7 @@
             // Handle error: Show error message to the user
           });
       },
+
       submitForm() {
         this.submitted = true;
         axios.post('/api/adminuser-login', this.employee)
