6174fc3ba94e94ad33e502861e393422b45a7dde
romit <romitu89@gmail.com>
2024-06-16 18:54:20 +0530

    SO-DE-131: componenent logic done

--- resources/js/Components/Editor/Client/ClientViewForm.vue
+++ resources/js/Components/Editor/Client/ClientViewForm.vue
@@ -1,145 +1,165 @@
 <template>
-  <div>
-    <form @submit.prevent="submitForm">
-      <table class="input_form">
-        <tr>
-          <td><label>Select Location</label></td>
-          <td>
-            <select v-model="client.selectedLocation" @blur="checkValidation('selectedLocation')" name="location">
-              <option value="">Select Location</option>
-              <option v-for="location in locations" :key="location" :value="location">{{ location }}</option>
-            </select><br>
-            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td><label>Sub Location</label></td>
-          <td>
-            <select v-model="client.selectedSubLocation" @blur="checkValidation('selectedSubLocation')" name="subLocation">
-              <option value="">Select Sub Location</option>
-              <option v-for="subLocation in subLocations" :key="subLocation" :value="subLocation">{{ subLocation }}</option>
-            </select><br>
-            <span v-if="errors.selectedSubLocation" class="error">{{ errors.selectedSubLocation[0] }}</span>
-          </td>
-        </tr>
-        <tr>
-      <td></td>
-     <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-      <button class="submit_btn">Submit</button> </td>
-  </tr>
-      </table>
-    </form>
-    <div v-if="results">
-      <responsive-table :results="results" :columns="columns" :buttonAction="buttonAction">
-    <template #edit="{ row }">
-      <div>
-        <button title="Edit" @click="editItem(row.client_id)">
-          <font-awesome-icon :icon="['fas', 'pen-to-square']" />
-        </button>
-        <button title="Delete" @click="deleteItem(row.client_id)">
-          <font-awesome-icon :icon="['fas', 'trash']" />
-        </button>
+    <div>
+      <form @submit.prevent="submitForm">
+        <table class="input_form">
+          <tr>
+            <td><label>Select Location</label></td>
+            <td>
+              <select v-model="client.selectedLocation" @change="updateFilteredSubLocations" @blur="checkValidation('selectedLocation')" name="location">
+                <option value="">Select Location</option>
+                <option v-for="location in uniqueLocations" :key="location" :value="location">{{ location }}</option>
+              </select><br>
+              <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
+            </td>
+          </tr>
+          <tr v-if="client.selectedLocation">
+            <td><label>Sub Location</label></td>
+            <td>
+              <select v-model="client.selectedSubLocation" @blur="checkValidation('selectedSubLocation')" name="subLocation">
+                <option value="">Select Sub Location</option>
+                <option v-for="subLocation in filteredSubLocations" :key="subLocation" :value="subLocation">{{ subLocation }}</option>
+              </select><br>
+              <span v-if="errors.selectedSubLocation" class="error">{{ errors.selectedSubLocation[0] }}</span>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <button @click="closePopup()" class="cancel_btn">Cancel</button>
+              <button class="submit_btn">Submit</button>
+            </td>
+          </tr>
+        </table>
+      </form>
+      <div v-if="results.length">
+        <responsive-table :results="results" :columns="columns" :buttonAction="buttonAction">
+          <template #edit="{ row }">
+            <div>
+              <button title="Edit" @click="editItem(row.client_id)">
+                <font-awesome-icon :icon="['fas', 'pen-to-square']" />
+              </button>
+              <button title="Delete" @click="deleteItem(row.client_id)">
+                <font-awesome-icon :icon="['fas', 'trash']" />
+              </button>
+            </div>
+          </template>
+        </responsive-table>
       </div>
-    </template>
-  </responsive-table>
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-import { commonFunctionsMixin } from '../../../function.js';
-import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue'
-import Swal from 'sweetalert2'
-import axios from 'axios'
+  <script>
+  import { commonFunctionsMixin } from '../../../function.js';
+  import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue';
+  import Swal from 'sweetalert2';
+  import axios from 'axios';
 
-export default {
-  name: 'ClientViewForm',
-  mixins:[commonFunctionsMixin],
-  components: { ResponsiveTable },
-  data() {
-    return {
-      buttonAction: false,
-      client: {
-        selectedSubLocation: '',
-        selectedLocation: '',
+  export default {
+    name: 'ClientViewForm',
+    mixins: [commonFunctionsMixin],
+    components: { ResponsiveTable },
+    data() {
+      return {
+        buttonAction: false,
+        client: {
+          selectedSubLocation: '',
+          selectedLocation: '',
+        },
+        locations: [],
+        subLocations: [],
+        filteredSubLocations: [],
+        uniqueLocations: [],
+        errors: {},
+        results: [],
+        columns: [
+          { label: 'Client Name', key: 'client_name' },
+          { label: 'Business Unit Name', key: 'business_unit_name' },
+          { label: 'Sub Location', key: 'sub_location' },
+          { label: 'Select Location', key: 'location' },
+          { label: 'Client Manager', key: 'client_manager_name' },
+          { label: 'Client Manager Email ID', key: 'client_manager_email' },
+          { label: 'Edit', key: 'edit' },
+        ],
+      };
+    },
+    props: {
+      empName: {
+        type: String,
+        default: '',
       },
-      locations: [],
-      subLocations: [],
-      errors: {},
-      results: [],
-      columns: [
-        { label: 'Client Name', key: 'client_name' },
-        { label: 'Business Unit Name', key: 'business_unit_name' },
-        { label: 'Sub Location', key: 'sub_location' },
-        { label: 'Select Location', key: 'location' },
-        { label: 'Client Manager', key: 'client_manager_name' },
-        { label: 'Client Manager Email ID', key: 'client_manager_email' },
-        { label: 'Edit', key: 'edit' }
-      ]
-    };
-  },
-
-  props:{
-    empName:{
-    type: String,
-    default: "",
     },
-  },
-
-  methods: {
+    methods: {
       closePopup() {
-    this.$emit("closePopup");
-  },
-
-  editItem(id) {
-    this.$emit("updateForm", id);
-    console.log(id,"client view form")
-  },
-
-    userLocationApi() {
-      axios.get('/api/editorclient-view')
-  .then(response => {
-    this.locations = response.data.locations
-      .filter(location => location.location) // Filter out blank or null locations
-      .map(location => location.location);
-
-    this.subLocations = response.data.subLocations
-      .filter(subLocation => subLocation.sub_location) // Filter out blank or null sub-locations
-      .map(subLocation => subLocation.sub_location);
-  })
-    },
-    submitForm() {
-      axios.post('/api/editorclient-view', this.client)
-        .then(response => {
-          this.results = response.data.results;
-          this.errors = {};
-          if (Object.values(this.errors).length == 0) {
-          this.buttonAction = true;
+        this.$emit('closePopup');
+      },
+      checkValidation(fieldName) {
+        if (this.errors[fieldName]) {
+          delete this.errors[fieldName];
         }
+      },
+      updateFilteredSubLocations() {
+        this.filteredSubLocations = this.subLocations
+          .filter((subLocation) => subLocation.location === this.client.selectedLocation)
+          .map((subLocation) => subLocation.sub_location);
+        this.client.selectedSubLocation = '';
+        this.checkValidation('selectedLocation');
+      },
+      userLocationApi() {
+        axios
+          .get('/api/editorclient-view')
+          .then((response) => {
+            const locationData = response.data.locations;
 
-        })
-        .catch(error => {
-          console.error('Error submitting form:', error);
-          this.errors = error.response.data.errors;
+            // Extract unique locations
+            this.uniqueLocations = [...new Set(locationData.map((loc) => loc.location))];
 
-        });
-    }
-  },
-  mounted() {
-  this.userLocationApi();
-
-  if(this.empName!=""){
-    Swal.fire({
-            position: "top-center",
-            icon: "success",
-            title: "User "+this.empName+" edited successfully",
-            showConfirmButton: false,
-            timer: 3000,
+            // Extract sub-locations
+            this.subLocations = locationData;
+          })
+          .catch((error) => {
+            console.error('Error fetching locations:', error);
           });
-          this.$emit("editMessageUpdated")
-  }
+      },
+      submitForm() {
+        axios
+          .post('/api/editorclient-view', this.client)
+          .then((response) => {
+            this.results = response.data.results;
+            this.errors = {};
+            if (!Object.values(this.errors).length) {
+              this.buttonAction = true;
+              this.client = {
+                selectedSubLocation: '',
+                selectedLocation: '',
+              };
+            }
+          })
+          .catch((error) => {
+            this.errors = error.response.data.errors;
+          });
+      },
+      editItem(id) {
+        this.$emit('updateForm', id);
+        console.log(id, 'client view form');
+      },
+    },
+    mounted() {
+      this.userLocationApi();
 
+      if (this.empName !== '') {
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: `User ${this.empName} edited successfully`,
+          showConfirmButton: false,
+          timer: 3000,
+        });
+        this.$emit('editMessageUpdated');
+      }
+    },
+  };
+  </script>
 
-},
-};
-</script>
+  <style scoped>
+  /* Your component-specific styles */
+  </style>
--- resources/js/Components/Editor/User/EditorLoginForm.vue
+++ resources/js/Components/Editor/User/EditorLoginForm.vue
@@ -1,163 +1,165 @@
 <template>
-
     <form @submit.prevent="submitForm">
-        <table class="input_form">
-
+      <table class="input_form">
         <tr>
-        <td ><label >Select Location</label></td>
-        <td ><select id="location" v-model="employee.selectedLocation"  @blur="checkValidation('selectedLocation')" name="location">
-        <option value="">Select Location</option>
-        <option v-for="item in getUniqueValues(loginData,'location')" :key='item' :value="item">{{ item }}</option>
-
-        </select><br><span v-if="errors.selectedLocation" class="error">{{errors.selectedLocation[0]}}</span></td>
+          <td><label>Select Location</label></td>
+          <td>
+            <select id="location" v-model="employee.selectedLocation" @change="updateFilteredData" @blur="checkValidation('selectedLocation')" name="location">
+              <option value="">Select Location</option>
+              <option v-for="item in getUniqueValues(loginData, 'location')" :key="item" :value="item">{{ item }}</option>
+            </select>
+            <br>
+            <span v-if="errors.selectedLocation" class="error">{{ errors.selectedLocation[0] }}</span>
+          </td>
         </tr>
-
-        <tr>
-        <td ><label >Employee Email</label></td>
-        <td ><select id="email" v-model="employee.selectedEmail"  @blur="checkValidation('selectedEmail')" name="email">
-        <option value="">Select Email</option>
-        <option v-for="item in getUniqueValues(loginData,'email_id')"
-         :key='item' :value="item">{{ item }}</option>
-
-        </select><br>
-        <span v-if="errors.selectedEmail" class="error">{{errors.selectedEmail[0]}}</span></td>
+        <tr v-if="employee.selectedLocation">
+          <td><label>Employee Email</label></td>
+          <td>
+            <select id="email" v-model="employee.selectedEmail" @change="resetFormFields('email')" @blur="checkValidation('selectedEmail')" name="email">
+              <option value="">Select Email</option>
+              <option v-for="item in filteredEmails" :key="item" :value="item">{{ item }}</option>
+            </select>
+            <br>
+            <span v-if="errors.selectedEmail" class="error">{{ errors.selectedEmail[0] }}</span>
+          </td>
         </tr>
-
-        <tr>
-        <td ><label >From Date</label></td>
-        <td ><input type="date" v-model="employee.fromDate"  @blur="checkValidation('fromDate')" >
-            <span v-if="errors.fromDate" class="error">{{errors.fromDate[0]}}</span></td>
+        <tr v-if="employee.selectedEmail">
+          <td><label>From Date</label></td>
+          <td>
+            <input type="date" v-model="employee.fromDate" @change="resetToDate" @blur="checkValidation('fromDate')">
+            <span v-if="errors.fromDate" class="error">{{ errors.fromDate[0] }}</span>
+          </td>
         </tr>
-
-        <tr>
-        <td ><label >To Date</label></td>
-        <td ><input type="date" v-model="employee.toDate"  @blur="checkValidation('toDate')" >
-            <span v-if="errors.toDate" class="error">{{errors.toDate[0]}}</span></td>
+        <tr v-if="employee.fromDate">
+          <td><label>To Date</label></td>
+          <td>
+            <input type="date" v-model="employee.toDate" :min="employee.fromDate" @blur="checkValidation('toDate')">
+            <span v-if="errors.toDate" class="error">{{ errors.toDate[0] }}</span>
+          </td>
         </tr>
-
         <tr>
-        <td></td>
-       <td> <button @click="closePopup()" class="cancel_btn">Cancel</button>
-        <button class="submit_btn">Submit</button> </td>
-    </tr>
-
-    </table>
+          <td></td>
+          <td>
+            <button @click="closePopup()" class="cancel_btn">Cancel</button>
+            <button type="submit" class="submit_btn">Submit</button>
+          </td>
+        </tr>
+      </table>
     </form>
+    <div v-if="results.length">
+      <responsive-table :results="formattedResults" :columns="columns" :buttonAction="buttonAction"></responsive-table>
+    </div>
+  </template>
 
-    <div v-if="results">
-                    <responsive-table :results="results" :columns="columns" :buttonAction="buttonAction">
+  <script>
+  import Swal from 'sweetalert2';
+  import { commonFunctionsMixin } from '../../../function.js';
+  import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue';
 
-
-                      <!-- <template #edit="{ row }">
-                        <Link
-
-                          >Edit</Link
-                        >
-                      </template> -->
-                    </responsive-table>
-                  </div>
-
-    </template>
-
-    <script>
-    import Swal from 'sweetalert2'
-    import { commonFunctionsMixin } from '../../../function.js';
-    import ResponsiveTable from '../../Shared Folder/ResponsiveTable.vue'
-
-
-
-        export default {
-            name: 'UserLoginForm',
-
-            mixins:[commonFunctionsMixin],
-
-            components:{
-            ResponsiveTable,
-            },
-
-            data()
-            {
-                return{
-                    employee:{
-                        selectedLocation:"",
-                        selectedEmail:"",
-                        fromDate:"",
-                        toDate:"",
-                    },
-                    buttonAction: false,
-                    loginData:[],
-                    userLocation:[],
-                    errors:{},
-                    results:[],
-                    columns: [
-                    { label: 'Email ID', key: 'email_id' },
-                    { label: 'Location', key: 'location' },
-                    { label: 'Created At', key: 'created_at' },
-                    { label: 'Logout', key: 'logout_time' },
-              ],
-                };
-            },
-
-
-            methods: {
-                closePopup() {
-      this.$emit("closePopup");
+  export default {
+    name: 'UserLoginForm',
+    mixins: [commonFunctionsMixin],
+    components: {
+      ResponsiveTable,
     },
-    checkValidation(fieldName) {
-      let dataError = Object.values(this.errors);
-      if (dataError.length > 1) {
-        this.submitForm();
-      } else {
-        if (this.errors.hasOwnProperty(fieldName)) {
+    data() {
+      return {
+        employee: {
+          selectedLocation: "",
+          selectedEmail: "",
+          fromDate: "",
+          toDate: "",
+        },
+        buttonAction: false,
+        loginData: [],
+        results: [],
+        errors: {},
+        columns: [
+          { label: 'Email ID', key: 'email_id' },
+          { label: 'Location', key: 'location' },
+          { label: 'Created At', key: 'formatted_created_at' },
+          { label: 'Logout Time', key: 'formatted_logout_time' },
+        ],
+        filteredEmails: [],
+      };
+    },
+    computed: {
+      formattedResults() {
+        return this.results.map((result) => ({
+          ...result,
+          formatted_created_at: this.formatDateTime(result.created_at),
+          formatted_logout_time: this.formatDateTime(result.logout_time),
+        }));
+      },
+    },
+    methods: {
+      closePopup() {
+        this.$emit("closePopup");
+      },
+      checkValidation(fieldName) {
+        if (this.errors[fieldName]) {
           delete this.errors[fieldName];
         }
-      }
-    },
-
-                userLocationApi()
-            {
-                axios
+      },
+      resetFormFields(field) {
+        switch (field) {
+          case 'location':
+            this.employee.selectedEmail = "";
+            this.employee.fromDate = "";
+            this.employee.toDate = "";
+            break;
+          case 'email':
+            this.employee.fromDate = "";
+            this.employee.toDate = "";
+            break;
+        }
+      },
+      resetToDate() {
+        this.employee.toDate = "";
+      },
+      updateFilteredData() {
+        this.filteredEmails = this.getUniqueValues(this.loginData.filter(item => item.location === this.employee.selectedLocation), 'email_id');
+        this.resetFormFields('location');
+      },
+      getUniqueValues(data, key) {
+        return [...new Set(data.map(item => item[key]))];
+      },
+      userLocationApi() {
+        axios
           .get('/api/editoruser-login')
           .then(response => {
-            this.loginData = response.data.login
-
-            console.log(this.loginData)
+            this.loginData = response.data.login;
           })
           .catch(error => {
-            console.log(error)
-            this.errored = true
-          })
-
-            },
-
-
-            submitForm() {
-                    this.submitted = true; // Set the submitted flag to true when attempting to submit the form
-                    // if (this.isFormValid) {
-
-                        axios.post('/api/editoruser-login', this.employee)
-                  .then(response => {
-                      console.log('Form submitted:', response.data.results);
-
-                      this.results = response.data.results;
-                  this.errors={};
-
-                    if (Object.values(this.errors).length == 0) {
-            this.buttonAction = true;
-              }
-
-               })
-              .catch(error => {
-                //   console.error('Error submitting form:', error.response.data.errors);
-                  this.errors= error.response.data.errors;
-               });
-    },
-        },
-        mounted(){
-    this.userLocationApi()
-
-}
-
+            console.error('Error fetching user locations:', error);
+          });
+      },
+      submitForm() {
+        axios.post('/api/editoruser-login', this.employee)
+          .then(response => {
+            this.results = response.data.results;
+            this.errors = {};
+            if (!Object.values(this.errors).length) {
+              this.buttonAction = true;
+              this.employee = {
+                selectedLocation: "",
+                selectedEmail: "",
+                fromDate: "",
+                toDate: "",
+              };
             }
-    </script>
+          })
+          .catch(error => {
+            this.errors = error.response.data.errors;
+          });
+      },
+    },
+    mounted() {
+      this.userLocationApi();
+    },
+  };
+  </script>
 
+  <style scoped>
+  /* Your component-specific styles */
+  </style>
